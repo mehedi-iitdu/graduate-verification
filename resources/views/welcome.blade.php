c733010c016ea7a9efd05edfb4fb293340c84f9d
Tanvir Ahassan Anik <anik880@gmail.com>
2017-09-27 14:10:29 +0600

    Correction

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hello BSSE 6th Batch
+                    Hello BSSE 6th Batch!
                 </div>
 
                 <div class="links">
