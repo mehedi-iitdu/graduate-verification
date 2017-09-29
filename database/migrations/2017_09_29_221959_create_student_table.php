932a15b1a4ddecb3287fc8ca068cb72848bf252d
Md. Mehedi Hasan <bsse0607@iit.du.ac.bd>
2017-09-30 05:16:13 +0600

    Migration
    All DB tables are created

---
+++ database/migrations/2017_09_29_221959_create_student_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('university_id');
+            $table->integer('department_id');
+            $table->string('registration_no');
+            $table->string('session');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student');
+    }
+}
