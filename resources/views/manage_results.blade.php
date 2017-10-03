e15d9118ed4888645da013154415770a758fcf0a
Feroz Ahmmed <froghramar@gmail.com>
2017-10-03 19:31:46 +0600

    update manage_users view

--- resources/views/manage_results.blade.php
+++ resources/views/manage_results.blade.php
@@ -55,109 +55,93 @@
             </nav>
 
             <main class="col-sm-9 ml-sm-auto col-md-10 pt-3" role="main">
-                <h2 style="margin-bottom: 40px" class="d-none d-sm-block">Manage Users</h2>
+                <h2 style="margin-bottom: 40px" class="d-none d-sm-block">Manage Results</h2>
 
                 <div class="jumbotron">
                     <form>
-                        <div class="form-row">
-                            <div class="form-group col-md-3">
-                                <select class="form-control" id="user_role">
-                                    <option>Select Role</option>
-                                    <option>DU</option>
-                                    <option>JU</option>
-                                    <option>NSU</option>
-                                    <option>PO</option>
-                                    <option>DEO</option>
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label" for="user_role">University Name</label>
+                            <div class="col-sm-8">
+                                <select class="form-control" id="univ_name">
+                                    <option>University of Dhaka</option>
+                                    <option>University of Khulna</option>
                                 </select>
                             </div>
-                            <div class="form-group col-md-3">
-                                <select class="form-control" id="user_role">
-                                    <option>Select University</option>
-                                    <option>DU</option>
-                                    <option>JU</option>
-                                    <option>NSU</option>
-                                    <option>PO</option>
-                                    <option>DEO</option>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="email" class="col-sm-4 col-form-label">Department/Institute</label>
+                            <div class="col-sm-8">
+                                <select class="form-control" id="dept_name">
+                                    <option>Institute of Information Technology</option>
+                                    <option>Applied Physics</option>
                                 </select>
                             </div>
-                            <div class="form-group col-md-3">
+                        </div>
+
+
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label" for="user_role">Semester</label>
+                            <div class="col-sm-8">
                                 <select class="form-control" id="user_role">
-                                    <option>Select Department</option>
-                                    <option>DU</option>
-                                    <option>JU</option>
-                                    <option>NSU</option>
-                                    <option>PO</option>
-                                    <option>DEO</option>
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                    <option>6</option>
+                                    <option>7</option>
+                                    <option>8</option>
+                                    <option>9</option>
+                                    <option>10</option>
+                                    <option>11</option>
+                                    <option>12</option>
                                 </select>
                             </div>
-                            <div class="form-group col-md-3">
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="text" class="col-sm-4 col-form-label">Registration No.</label>
+                            <div class="col-sm-8">
+                                <input type="text" class="form-control" id="email" placeholder="Student Registration No.">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div align="right">
                                 <button type="submit" class="btn btn-success">Search</button>
-                                <button type="submit" class="btn btn-primary">Add user</button>
                             </div>
                         </div>
                     </form>
                 </div>
-
                 <div>
                     <table class="table table-bordered table-responsive">
                         <thead>
                         <tr>
                             <th>#</th>
-                            <th>Role</th>
-                            <th>Name</th>
-                            <th>University</th>
-                            <th>Department</th>
-                            <th>Email</th>
-                            <th>Action</th>
+                            <th>Course Name</th>
+                            <th>Course Code</th>
+                            <th>Credit</th>
+                            <th>GPA</th>
                         </tr>
                         </thead>
                         <tbody>
                         <tr>
                             <th scope="row">1</th>
-                            <td>UGC</td>
-                            <td>Otto</td>
-                            <td>DU</td>
-                            <td>IIT</td>
-                            <td>example@gmail.com</td>
+                            <td>Course 1</td>
+                            <td>801</td>
+                            <td>3</td>
                             <td>
-                                <button class="btn btn-primary">Edit</button>
-                                <button class="btn btn-danger">Delete</button>
+                                <input type="text" class="form-control" placeholder="GPA">
                             </td>
                         </tr>
                         <tr>
-                            <th scope="row">1</th>
-                            <td>UGC</td>
-                            <td>Otto</td>
-                            <td>DU</td>
-                            <td>IIT</td>
-                            <td>example@gmail.com</td>
+                            <th scope="row">2</th>
+                            <td>Course 2</td>
+                            <td>802</td>
+                            <td>3</td>
                             <td>
-                                <button class="btn btn-primary">Edit</button>
-                                <button class="btn btn-danger">Delete</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>UGC</td>
-                            <td>Otto</td>
-                            <td>DU</td>
-                            <td>IIT</td>
-                            <td>example@gmail.com</td>
-                            <td>
-                                <button class="btn btn-primary">Edit</button>
-                                <button class="btn btn-danger">Delete</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>UGC</td>
-                            <td>Otto</td>
-                            <td>DU</td>
-                            <td>IIT</td>
-                            <td>example@gmail.com</td>
-                            <td>
-                                <button class="btn btn-primary">Edit</button>
-                                <button class="btn btn-danger">Delete</button>
+                                <input type="text" class="form-control" placeholder="GPA">
                             </td>
                         </tr>
                         </tbody>
