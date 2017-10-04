5408a6a87519e15a6c193694f4622e347986c990
Tanvir Ahassan Anik <anik880@gmail.com>
2017-10-04 13:37:27 +0600

    Add sidebar in inc folder

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -15,3 +15,64 @@
     </ul>
   </div>
 </nav>
+
+
+// Nav sidebar
+<nav class="col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar">
+  <ul class="nav nav-pills flex-column">
+    <li class="nav-item">
+      <a class="nav-link active" href="manage_users_view">Manage Users</a>
+    </li>
+    <li class="nav-item">
+      <div class="dropdown show">
+        <a class="nav-link dropdown-toggle" href="#" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Manage Universities
+        </a>
+
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+          <a class="dropdown-item" href="manage_university_view">University</a>
+          <a class="dropdown-item" href="#">Department/Institute</a>
+        </div>
+      </div>
+    </li>
+
+    <li class="nav-item">
+      <div class="dropdown show">
+        <a class="nav-link dropdown-toggle" href="#" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Manage Department/Institute
+        </a>
+
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+          <a class="dropdown-item" href="#">Semester</a>
+          <a class="dropdown-item" href="manage_courses_view">Course</a>
+        </div>
+      </div>
+    </li>
+
+    <li class="nav-item">
+      <div class="dropdown show">
+        <a class="nav-link dropdown-toggle" href="#" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Manage Result
+        </a>
+
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+          <a class="dropdown-item" href="#">Add Result</a>
+          <a class="dropdown-item" href="#">Edit</a>
+        </div>
+      </div>
+    </li>
+
+    <li class="nav-item">
+      <div class="dropdown show">
+        <a class="nav-link dropdown-toggle" href="#" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Manage Verification
+        </a>
+
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+          <a class="dropdown-item" href="#">Request</a>
+          <a class="dropdown-item" href="#">Verify</a>
+        </div>
+      </div>
+    </li>
+  </ul>
+</nav>
--- resources/views/user_dashboard/manage_users_view.blade.php
+++ resources/views/user_dashboard/manage_users_view.blade.php
@@ -4,74 +4,23 @@
 
     <div class="container-fluid">
       <div class="row">
-        <nav class="col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar">
-          <ul class="nav nav-pills flex-column">
-            <li class="nav-item">
-              <a class="nav-link active" href="#">Manage Users</a>
-            </li>
-            <li class="nav-item">
-              <div class="dropdown show">
-                <a class="nav-link dropdown-toggle" href="#" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Manage Universities
-                </a>
-
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                  <a class="dropdown-item" href="#">University</a>
-                  <a class="dropdown-item" href="#">Department/Institute</a>
-                </div>
-              </div>
-            </li>
-
-            <li class="nav-item">
-              <div class="dropdown show">
-                <a class="nav-link dropdown-toggle" href="#" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Manage Department/Institute
-                </a>
-
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                  <a class="dropdown-item" href="#">Semester</a>
-                  <a class="dropdown-item" href="#">Course</a>
-                </div>
-              </div>
-            </li>
-
-            <li class="nav-item">
-              <div class="dropdown show">
-                <a class="nav-link dropdown-toggle" href="#" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Manage Result
-                </a>
-
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                  <a class="dropdown-item" href="#">Add Result</a>
-                  <a class="dropdown-item" href="#">Edit</a>
-                </div>
-              </div>
-            </li>
-
-            <li class="nav-item">
-              <div class="dropdown show">
-                <a class="nav-link dropdown-toggle" href="#" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Manage Verification
-                </a>
-
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                  <a class="dropdown-item" href="#">Request</a>
-                  <a class="dropdown-item" href="#">Verify</a>
-                </div>
-              </div>
-            </li>
-          </ul>
-        </nav>
-
         <main class="col-sm-9 ml-sm-auto col-md-10 pt-3" role="main">
-          <h2 style="margin-bottom: 40px" class="d-none d-sm-block">Manage Users</h2>
+          <h2 style="margin-bottom: 20px" class="text-center d-none d-sm-block">Manage Users</h2>
 
-          <div class="jumbotron">
+          <hr>
+
+          <div class="form-row">
+              <div class="form-group ml-auto col-md-3">
+                  <a href="/dashboard/manage_users_create" class="btn btn-block btn-primary">Add User</a>
+              </div>
+          </div>
+
+          <div>
             <form>
               <div class="form-row">
                   <div class="form-group col-md-3">
                     <select class="form-control" id="user_role">
-                      <option>Select Role</option>
+                      <option>Select User Role</option>
                       <option>DU</option>
                       <option>JU</option>
                       <option>NSU</option>
@@ -100,8 +49,7 @@
                     </select>
                   </div>
                   <div class="form-group col-md-3">
-                    <button type="submit" class="btn btn-success">Search</button>
-                    <a href="manage_users_create" type="btn" class="btn btn-primary">Add User</a>
+                    <button type="submit" class="btn btn-block btn-success">Search</button>
                   </div>
                 </div>
             </form>
