f2a62765ab45aeafc1133dbe90ea16b19c895c15
Pritom Saha Akash <pritomsaha163@gmail.com>
2017-10-05 12:44:25 +0600

    Merge branch 'dev' into bsse0604
    models added

--- app/Course.php
+++ app/Course.php
@@ -3,10 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Marks;
 class Course extends Model
 {
     //
     protected $table = 'course';
+
+    public function marks(){
+    	return $this->hasMany(Marks::class);
+    }
     
 }
--- app/Student.php
+++ app/Student.php
@@ -3,10 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\University;
+use App\Department;
+use App\Marks;
 
 class Student extends Model
 {
     protected $table = 'student';
 
+    public function university(){
+        return $this->belongsTo(Department::class);
+    }
+
+    public function department(){
+        return $this->belongsTo(Department::class);
+    }
+
+    public function marks(){
+    	return $this->hasMany(Marks::class);
+    }
         
 }
