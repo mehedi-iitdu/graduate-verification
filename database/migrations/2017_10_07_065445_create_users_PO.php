5b611a3ea303b976b927e4188e8d3200829779dc
Md. Mehedi Hasan <bsse0607@iit.du.ac.bd>
2017-10-07 12:58:39 +0600

    Auto stash before merge of "dev" and "origin/dev"

--- database/migrations/2017_10_07_065445_create_users_PO.php
+++ database/migrations/2017_10_07_065445_create_users_PO.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateUsersPO extends Migration
 {
     /**
      * Run the migrations.
@@ -13,18 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('po', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('mobile_no');
-            $table->integer('role_id');
-            $table->boolean('is_activated');
-            $table->integer('university_id');
             $table->integer('department_id');
             $table->timestamps();
+            $table->integer('user_id');
         });
     }
 
@@ -35,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('po');
     }
 }
