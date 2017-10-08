9d41a458be15a7a2c11d58a3568e7f463c0134c2
Tanvir Ahassan Anik <anik880@gmail.com>
2017-10-08 11:33:47 +0600

    mdbootstrap js hides

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,7 +31,7 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 
         {{-- MDBootstrap JS --}}
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.4.1/js/mdb.min.js"></script>
+        {{-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.4.1/js/mdb.min.js"></script> --}}
 
         {{-- Custom js --}}
         <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
