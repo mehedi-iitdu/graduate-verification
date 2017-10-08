de51cc31cc303c82a86bba865e41dcb4d763476f
Md. Mehedi Hasan <bsse0607@iit.du.ac.bd>
2017-10-08 12:00:57 +0600

    Documentation nai

---
+++ resources/views/user_dashboard/manage_department_view.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="container-fluid">
+    <div class="row">
+      <main class="col-sm-9 ml-sm-auto col-md-10 pt-3" role="main">
+        <h2 style="margin-bottom: 20px" class="d-none d-sm-block text-center">Manage Department</h2>
+        <hr>
+
+        
+      </main>
+    </div>
+  </div>
+@endsection
