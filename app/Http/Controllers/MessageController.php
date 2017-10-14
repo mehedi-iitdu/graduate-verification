9b01af7ef2715051adda0e3446ee8ec8d0d71e37
Pritom Saha Akash <pritomsaha163@gmail.com>
2017-10-15 03:08:48 +0600

    changed

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -3,11 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use App\Verification;
 
 class MessageController extends Controller
 {
-    public function showMessage(){ 
-      return view('message.view');
+
+	public function __construct(){
+
+		$this->middleware('auth');
+	}
+
+    public function showMessage(Request $request){
+
+    	if($request->user()->role == "Student") {
+    		$verifications = Verification::where('student_id', $request->user()->id);
+    		return view('message.view', ['messages' => $verifications]);
+    	}
+    	/*return view('message.view');*/
     }
 
     public function showSingleMessage(){ 
--- resources/views/message/view.blade.php
+++ resources/views/message/view.blade.php
@@ -6,32 +6,15 @@
       <div class="row">
         <main class="col-sm-9 ml-sm-auto col-md-10 pt-3" role="main">
           <div class="list-group">
-            <a href="{{ URL::route('message.single') }}" class="list-group-item list-group-item-action flex-column align-items-start active">
-              <div class="d-flex w-100 justify-content-between">
-                <h5 class="mb-1">Request Decline</h5>
-                <small>3 days ago</small>
-              </div>
-              <p class="mb-1">From: <span>Abdul Matin</span></p>
-              <p class="mb-1">Date: <span>03-04-2017</span></p>
-            </a>
-
-            <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
-              <div class="d-flex w-100 justify-content-between">
-                <h5 class="mb-1">Payment Request</h5>
-                <small>15 days ago</small>
-              </div>
-              <p class="mb-1">From: <span>Abdul Matin</span></p>
-              <p class="mb-1">Date: <span>03-04-2017</span></p>
-            </a>
-
-            <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
-              <div class="d-flex w-100 justify-content-between">
-                <h5 class="mb-1">Confirm Request</h5>
-                <small>3 days ago</small>
-              </div>
-              <p class="mb-1">From: <span>Abdul Matin</span></p>
-              <p class="mb-1">Date: <span>03-04-2017</span></p>
-            </a>
+            @foreach($messages as $message)
+              <a href="{{ URL::route('message.single') }}" class="list-group-item list-group-item-action flex-column align-items-start active">
+                <div class="d-flex w-100 justify-content-between">
+                  <h5 class="mb-1">{{$message -> verification_status}}</h5>
+                </div>
+                <p class="mb-1">From: <span>{{ $message->stakeholder->name }}</span></p>
+                <p class="mb-1">Date: <span>{{ $message->created_at }}</span></p>
+              </a>
+            @endforeach
           </div>
         </main>
       </div>
