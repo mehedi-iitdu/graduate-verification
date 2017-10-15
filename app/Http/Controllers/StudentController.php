23e175161889ad0866f9f7f5e196ae51a2905d3a
Feroz Ahmmed <froghramar@gmail.com>
2017-10-16 01:38:41 +0600

    solve student controller issue

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -170,7 +170,7 @@ class StudentController extends Controller
         return view('student.view');
     }
 
-    public function getStudentListByDepartment(Request $request, $id){
+    public function getStudentListByDepartment(Request $request){
 
         $students = \DB::table('student')
             ->select('student.*','user.*')
