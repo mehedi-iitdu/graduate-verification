cb0abb5c691fef02bd163dd79c2b8f7bd7879310
Md. Mehedi Hasan <bsse0607@iit.du.ac.bd>
2017-10-16 16:42:49 +0600

    Changed

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -15,31 +15,6 @@ class DashboardController extends Controller
 
     public function dashboardView(Request $request) {
 
-        /*if($request->user()->role == "Student") {
-            $verifications = Verification::where('student_id', $request->user()->id)->get();
-            return view('dashboard', ['message' => count($verifications)]);
-        }
-
-        elseif($request->user()->role == "Registrar") {
-            $university = Registrar::where('user_id',$request->user()->id)->first()->university;
-            $department_ids = Department::where('university_id', $university->id)->pluck('id');
-            $student_ids = Student::whereIn('department_id', $department_ids)->pluck('id');
-            $verifications = Verification::whereIn('student_id', $student_ids)->get();
-            return view('dashboard', ['message' => count($verifications)]);
-        }
-
-        elseif($request->user()->role == "ProgramOffice") {
-            $department = ProgramOffice::where('user_id',$request->user()->id)->first()->department;
-            $student_ids = Student::where('department_id', $department->id)->pluck('id');
-            $verifications = Verification::whereIn('student_id', $student_ids)->get();
-            return view('dashboard', ['message' => count($verifications)]);
-        }
-
-        elseif($request->user()->role == "UGC") {
-            $verifications = Verification::get();
-            return view('dashboard', ['message' => count($verifications)]);
-        }*/
-
         return view('dashboard');
     }
 
