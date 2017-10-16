7672d2dbb2246b96c393540a7cb8c7f0da06f43a
Mehedi Hasan <bsse0607@iit.du.ac.bd>
2017-10-17 03:09:36 +0600

    User add bug fixed

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -43,7 +43,7 @@ class RegisterController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth')->only(['showRegistrationForm', 'storeUser']);
+        $this->middleware('role:SystemAdmin, UGC, Registrar')->only(['showRegistrationForm', 'storeUser']);
         $this->middleware('guest')->only(['showActivationForm', 'userActivate',
             'showSendActivationCodeForm', 'activationCodeSend', 'sendActivationCode']);
     }
